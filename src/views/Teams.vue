f423a6da314b97034761edc646ff600312caaf80
Jordan <jordangill600@gmail.com>
2019-06-05 23:30:54 +0100

    Add updates

--- src/views/Teams.vue
+++ src/views/Teams.vue
@@ -1,11 +1,21 @@
 <template>
-  <div></div>
+  <div>
+    <Header/>
+    <p>this is the teams page</p>
+    <Footer/>
+  </div>
 </template>
 
 <script>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 export default {
   name: "Teams",
-  components: {}
+  components: {
+    Header,
+    PageHeader,
+    Footer
+  }
 };
 </script>
 
