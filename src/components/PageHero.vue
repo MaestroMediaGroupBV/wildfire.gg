3a5bf4b1050897a0739b495ad06d32394321dae0
Jordan <jordangill600@gmail.com>
2019-06-06 01:53:01 +0100

    Add updates

--- src/components/PageHero.vue
+++ src/components/PageHero.vue
@@ -1,12 +1,57 @@
 <template>
-  <div></div>
+  <div>
+    <div class="hero">
+      <div class="container">
+        <div class="title">OUR TEAMS</div>
+        <div class="subTitle">LINE UPS</div>
+        <button>MORE INFO</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "PageHeader"
+  name: "PageHero"
 };
 </script>
 
 <style lang="scss" scoped>
+.container {
+  padding-left: 200px;
+  padding-right: 200px;
+}
+.hero {
+  background-image: url("../assets/images/team-header.png");
+  height: 500px;
+  margin-top: -25px;
+}
+
+.title {
+  @include storeTitle;
+  padding-left: 70px;
+  text-align: left;
+}
+
+.subTitle {
+  @include storeSubTitle;
+  margin-top: -45px;
+  text-align: left;
+}
+
+button {
+  @include heroButton;
+  margin-top: 100px;
+  padding: 0px;
+  height: 40px;
+  border-top: 0px;
+  border-right: 0px;
+  border-left: 0px;
+  border-bottom: solid $white 4px;
+  border-radius: 0px;
+  cursor: pointer;
+  outline: none;
+
+  background-color: Transparent;
+}
 </style>
--- src/views/Teams.vue
+++ src/views/Teams.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <Header/>
-    <p>this is the teams page</p>
+
+    <PageHero/>
+    <div class="container"></div>
     <Footer/>
   </div>
 </template>
@@ -9,15 +11,25 @@
 <script>
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
+import PageHero from "@/components/PageHero.vue";
 export default {
   name: "Teams",
   components: {
     Header,
-    PageHeader,
+    PageHero,
     Footer
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.container {
+  padding-left: 200px;
+  padding-right: 200px;
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+}
 </style>
