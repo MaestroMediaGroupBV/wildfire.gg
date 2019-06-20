6886390c9a4ecf1ff943582679b3d38894c9747c
Jordan <jordangill600@gmail.com>
2019-06-20 05:22:19 +0100

    Add pageclip & snipcart  🚀

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,32 @@
     <About/>
     <StoreBanner/>
     <StoreProducts/>
+    <button
+      class="snipcart-add-item"
+      data-item-id="2"
+      data-item-name="Bacon"
+      data-item-price="3.00"
+      data-item-weight="20"
+      data-item-url="http://myapp.com/products/bacon"
+      data-item-description="Some fresh bacon"
+    >Buy bacon</button>
+    <form
+      action="https://send.pageclip.co/z5cyTLSpCQkajtu0I12L8QuHsneqeLhC/contact-form"
+      class="pageclip-form"
+      method="post"
+    >
+      <!-- Replace these inputs with your own. Make sure they have a "name" attribute! -->
+
+      <!-- It looks like you are creating a contact form. These email and subject fields are special -->
+      <!-- See https://pageclip.co/docs#special-fields for more info -->
+      <input type="email" name="email" value="roscoe@example.com">
+      <input type="text" name="subject" value="A contact subject">
+
+      <!-- This button will have a loading spinner. Keep the inner span for best results. -->
+      <button type="submit" class="pageclip-form__submit">
+        <span>Send</span>
+      </button>
+    </form>
     <ResultsSlider/>
     <Calendar/>
     <Contact/>
